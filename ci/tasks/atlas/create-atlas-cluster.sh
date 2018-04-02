0883bd3cefb007892970316c8a7297ce8f004247
Sani Chabi Yo <chabiyosani@gmail.com>
2018-04-02 15:16:42 -0400

    add logic to query cluster creating status

--- ci/tasks/atlas/create-atlas-cluster.sh
+++ ci/tasks/atlas/create-atlas-cluster.sh
@@ -3,13 +3,13 @@ set -e -x
 
 
 source source-code/ci/tasks/atlas/create-atlas-project.sh
-
+source source-code/ci/tasks/atlas/get-cluster.sh
 
 createProject $ATLAS_USERNAME $ATLAS_API_KEY $ATLAS_ORG_ID $ATLAS_PROJECT_NAME projectId
 
 CURL_COMMAND="curl -u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: application/json' -X POST 'https://cloud.mongodb.com/api/atlas/v1.0/groups/ATLAS_PROJECT_ID/clusters' --data '
 {
-  \"name\" : \"MongoCQRS\",
+  \"name\" : \"ATLAS_CLUSTER_NAME\",
   \"diskSizeGB\" : 160,
   \"numShards\" : 1,
   \"providerSettings\" : {
@@ -28,10 +28,15 @@ CURL_COMMAND="curl -u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: 
 NEW_CURL_COMMAND=$(sed  "s@ATLAS_API_KEY@${ATLAS_API_KEY}@g" <<< $CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@ATLAS_PROJECT_ID@${projectId}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s~ATLAS_USERNAME~${ATLAS_USERNAME}~g" <<< $NEW_CURL_COMMAND)
+NEW_CURL_COMMAND=$(sed  "s~ATLAS_CLUSTER_NAME~${ATLAS_CLUSTER_NAME}~g" <<< $NEW_CURL_COMMAND)
+
 
 result=$(eval curl $NEW_CURL_COMMAND)
   if [[ $result == *"error"* ]]; then
       echo $result
+      if [[ $result == *"GROUP_ALREADY_EXISTS"* ]]; then
+          echo "Cluster already exist"
+          exit 0
       exit 1
   else
        access_token=$(jq .id <<< $result)
@@ -39,4 +44,27 @@ result=$(eval curl $NEW_CURL_COMMAND)
        TRIMMED_RESULT="${TRIMMED_RESULT#\"}"
        eval $responsevar="'$TRIMMED_RESULT'"
   fi
+
+echo "Waiting until Cluster is successfully created ";
+
+#Try for a maximum of 5 minutes
+## sleep in bash for loop ##
+for i in {1..5}
+do
+   #Get the cluster Status
+   getCluster $ATLAS_USERNAME $ATLAS_API_KEY projectId $ATLAS_CLUSTER_NAME state
+   echo "provisioningState:"$state
+   if [[ $state == "IDLE" ]]; then
+     portal_url=$(jq .mongoURI <<< $result)
+     MESSAGE="Cluster was successully created and can be accessed using the following URL:${portal_url}" ; simple_green_echo
+     exit 0
+   elif (( $state == "CREATING" )); then
+     echo "Waiting..."
+     sleep 1m
+   else
+     #The creation failes for a raison
+     echo"==>The cluster provisioning failed for a raison.";
+     exit 1
+   fi
+done
 
---
+++ ci/tasks/atlas/get-cluster.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+## @file                get-cluster.sh
+## @author              Sani Chabi Yo
+## @section DESCRIPTION A script which get information on an Atlas cluster
+
+
+# gloabal variables
+createProject(){
+  CURL_COMMAND="-u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: application/json' -X POST 'https://cloud.mongodb.com/api/atlas/v1.0/groups/GROUP-ID/clusters/CLUSTER-NAME'"
+  # local variable x and y with passed args
+  local username=$1
+  local api_key=$2
+  local group_id=$3
+  local cluster_name=$4
+  local status=$5
+
+  NEW_CURL_COMMAND=$(sed  "s@ATLAS_API_KEY@${api_key}@g" <<< $CURL_COMMAND)
+  NEW_CURL_COMMAND=$(sed  "s@GROUP-ID@${group_id}@g" <<< $NEW_CURL_COMMAND)
+  NEW_CURL_COMMAND=$(sed  "s@CLUSTER-NAME@${cluster_name}@g" <<< $NEW_CURL_COMMAND)
+  NEW_CURL_COMMAND=$(sed  "s~ATLAS_USERNAME~${username}~g" <<< $NEW_CURL_COMMAND)
+
+  result=$(eval curl $NEW_CURL_COMMAND)
+  if [[ $result == *"error"* ]]; then
+     echo $result
+     exit 1
+  else
+      access_token=$(jq .stateName <<< $result)
+      TRIMMED_RESULT="${access_token%\"}"
+      TRIMMED_RESULT="${TRIMMED_RESULT#\"}"
+      eval $responsevar="'$TRIMMED_RESULT'"
+  fi
+}
