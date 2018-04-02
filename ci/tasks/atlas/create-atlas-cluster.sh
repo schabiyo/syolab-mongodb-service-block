7bbd80e12d82018fad7d95124cb3d704e61d96c7
Sani Chabi Yo <chabiyosani@gmail.com>
2018-04-02 14:20:18 -0400

    fix curl

--- ci/tasks/atlas/create-atlas-cluster.sh
+++ ci/tasks/atlas/create-atlas-cluster.sh
@@ -9,22 +9,34 @@ createProject $ATLAS_USERNAME $ATLAS_API_KEY $ATLAS_PROJECT_NAME projectId
 
 CURL_COMMAND="curl -u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: application/json' -X POST 'https://cloud.mongodb.com/api/atlas/v1.0/groups/ATLAS_PROJECT_ID/clusters' --data '
 {
-  'name; : 'MongoCQRS',
-  'diskSizeGB' : 160,
-  'numShards' : 1,
-  'providerSettings' : {
-    'providerName' : 'AWS',
-    'diskIOPS' : 480,
-    'encryptEBSVolume' : false,
-    'instanceSizeName' : 'M10',
-    'regionName' : 'US_EAST_1'
+  \"name\" : \"MongoCQRS\",
+  \"diskSizeGB\" : 160,
+  \"numShards\" : 1,
+  \"providerSettings\" : {
+    \"providerName\" : \"AWS\",
+    \"diskIOPS\" : 480,
+    \"encryptEBSVolume\" : false,
+    \"instanceSizeName\" : \"M10\",
+    \"regionName\" : \"US_EAST_1\"
   },
-  'replicationFactor' : 3,
-  'replicationSpec':{'US_EAST_1':{'electableNodes':3,'priority':7,'readOnlyNodes':0}},
-  'backupEnabled' : false,
-  'autoScaling':{'diskGBEnabled':true}
+  \"replicationFactor\" : 3,
+  \"replicationSpec\":{\"US_EAST_1\":{\"electableNodes\":3,\"priority\":7,\"readOnlyNodes\":0}},
+  \"backupEnabled\": false,
+  \"autoScaling\":{\"diskGBEnabled\":true}
 }'"
 
 NEW_CURL_COMMAND=$(sed  "s@ATLAS_API_KEY@${ATLAS_API_KEY}@g" <<< $CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s@ATLAS_PROJECT_ID@${projectId}@g" <<< $NEW_CURL_COMMAND)
 NEW_CURL_COMMAND=$(sed  "s~ATLAS_USERNAME~${ATLAS_USERNAME}~g" <<< $NEW_CURL_COMMAND)
+
+result=$(eval curl $NEW_CURL_COMMAND)
+  if [[ $result == *"error"* ]]; then
+      echo $result
+      exit 1
+  else
+       access_token=$(jq .id <<< $result)
+       TRIMMED_RESULT="${access_token%\"}"
+       TRIMMED_RESULT="${TRIMMED_RESULT#\"}"
+       eval $responsevar="'$TRIMMED_RESULT'"
+  fi
+
--- ci/tasks/atlas/create-atlas-project.sh
+++ ci/tasks/atlas/create-atlas-project.sh
@@ -7,7 +7,7 @@
 
 # gloabal variables
 createProject(){
-  CURL_COMMAND="-u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: application/json' -X POST 'https://cloud.mongodb.com/api/atlas/v1.0/groups' --data '{ 'name' : 'ATLAS_PROJECT_NAME' }'"
+  CURL_COMMAND="-u 'ATLAS_USERNAME:ATLAS_API_KEY' --digest -H 'Content-Type: application/json' -X POST 'https://cloud.mongodb.com/api/atlas/v1.0/groups' --data '{ \"name\" : \"ATLAS_PROJECT_NAME\" }'"
   # local variable x and y with passed args
   local username=$1
   local api_key=$2
