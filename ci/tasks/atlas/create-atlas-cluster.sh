bd18e1bced13720fd6b3a2c3183587fe6593e617
Sani Chabi Yo <chabiyosani@gmail.com>
2018-04-02 12:52:38 -0400

    fix path 2

--- ci/tasks/atlas/create-atlas-cluster.sh
+++ ci/tasks/atlas/create-atlas-cluster.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 
-source syolab-mongodb-cqrs/ci/tasks/atlas/create-atlas-project.sh
+source source-code/ci/tasks/atlas/create-atlas-project.sh
 
 
 createProject $ATLAS_API_KEY $ATLAS_PROJECT_NAME projectId
